e4843cbfcd2e5a1b5fcc5e2bc5dbc2042d931d3e
Dietowski <81565239+Dietowski@users.noreply.github.com>
2024-09-06 10:50:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+function pesquisa() {
+    // Seleciona a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    campoPesquisa = campoPesquisa.toLowerCase();
+    if(campoPesquisa == ""){
+      section.innerHTML = "<p>Busca vazia. Nenhuma doença foi encontrada</p>"
+      return;
+    }
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+    let nome = "";
+    let descricao = "";
+    let sintomas = "";
+    let tratamento = "";
+    let fatores = "";
+    let tags = "";
+
+    // Itera sobre cada elemento do array 'dados' (assumindo que 'dados' seja um array de objetos com as propriedades 'nome', 'descricao', 'sintomas' e 'link')
+    for (let dado of dados) {
+      nome = dado.nome.toLowerCase();
+      descricao = dado.descricao.toLowerCase();
+      sintomas = dado.sintomas.toLowerCase();
+      tratamento = dado.tratamento.toLowerCase();
+      fatores = dado.fatoresDeRisco.toLowerCase();
+      tags = dado.tags.toLowerCase();
+
+      if(nome.includes(campoPesquisa) || descricao.includes(campoPesquisa) || sintomas.includes(campoPesquisa) || tratamento.includes(campoPesquisa) || fatores.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+        // Concatena HTML formatado para cada elemento, criando uma div com as informações da doença
+        resultados += `
+          <div class="item-resultado">
+            <h2><a href="#" target="_blank">${dado.nome}</a></h2> 
+            <p class="descricao-meta">${dado.descricao}</p> 
+            <p class="descricao-meta">${dado.sintomas}</p>
+            <p class="descricao-meta">${dado.fatoresDeRisco}</p>
+            <p class="descricao-meta">${dado.tratamento}</p>
+            <a href=${dado.link} target="_blank">Saiba mais</a> 
+          </div>
+        `;
+      }
+    }
+    if(!resultados){
+      resultados = "<p>Nenhuma doença foi encontrada</p>"
+    }
+    // Atribui o HTML gerado para a seção de resultados
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,29 @@
+let dados = [
+    {
+        nome : "Asma",
+        descricao : "A asma é uma doença crônica que afeta as vias aéreas, causando inflamação e obstrução. Essa inflamação faz com que as vias aéreas se estreitem, inchem e produzam muco em excesso, dificultando a passagem do ar e causando sintomas como:",
+        sintomas : "<strong>Sintomas</strong>: Tosse frequente, ruído ao respirar, falta de ar e aperto no peito.",
+        fatoresDeRisco: "<strong>Fatores de Risco</strong>: Tabagismo, poluição do ar, infecções virais, obesidade.",
+        tratamento: "<strong>Tratamento</strong>: Broncodilatadores, corticosteroides, imunoterapia, evitar gatilhos (alérgenos, irritantes), reabilitação pulmonar.",
+        link : "https://bvsms.saude.gov.br/asma/",
+        tags : "Tosse ruído ao respirar falta de ar aperto no peito"
+    },
+    {
+        nome: "Bronquite",
+        descricao: "A bronquite é uma inflamação dos brônquios, os tubos que transportam o ar para dentro e para fora dos pulmões. Essa inflamação causa tosse e produção de muco.",
+        sintomas: "<strong>Sintomas</strong>: Tosse persistente, produção de muco, fadiga.",
+        fatoresDeRisco: "<strong>Fatores de Risco</strong>: Fumar, exposição a irritantes, infecções virais.",
+        tratamento: "<strong>Tratamento</strong>: Repouso, hidratação, medicamentos para aliviar os sintomas.",
+        link: "https://www.minhavida.com.br/saude/temas/bronquite",
+        tags : "Tosse muco fadiga"
+      },
+    {
+        nome: "Pneumonia",
+        descricao: "A pneumonia é uma infecção dos alvéolos pulmonares, as pequenas bolsas de ar nos pulmões. Essa infecção causa inflamação e acúmulo de líquido nos pulmões.",
+        sintomas: "<strong>Sintomas</strong>: Tosse com produção de muco, febre, dor no peito, dificuldade para respirar.",
+        fatoresDeRisco: "<strong>Fatores de Risco</strong>: Idade avançada, sistema imunológico enfraquecido, doenças crônicas.",
+        tratamento: "<strong>Tratamento</strong>: Antibióticos, repouso, hidratação.",
+        link: "https://www.minhavida.com.br/saude/temas/pneumonia",
+        tags : "febre muco dor no peito falta de ar"
+    }
+];
